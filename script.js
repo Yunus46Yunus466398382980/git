79883f01a7f134ce7e4fab75c0567d25c68ec333
Yunus46Yunus466398382980 <munojat.1585@gmail.com>
2025-06-21 03:09:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const name     = prompt('Ismingizni kiriting');
+const id       = prompt('Space IDingizni kiriting');
+const password = prompt('Parolingizni kiriting');
+
+
+const userData = { name, id, password };
+
+console.log(userData); 
